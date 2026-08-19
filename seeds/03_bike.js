b25f2c7afa28672060194754fd90ea48b4a9c523
Cijid <mmkcijid@gmail.com>
2026-08-19 13:41:47 -0600

    Created fake data for bikes. Going to do people and components next

--- seeds/03_bike.js
+++ seeds/03_bike.js
@@ -4,9 +4,11 @@
  */
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
+
   await knex.schema.raw("TRUNCATE bike CASCADE");
   await knex("bike").del();
-  await knex("bike").insert([
+
+  const bikeSeed = [
     {
       id: 1,
       make: "Specialized",
@@ -34,5 +36,76 @@ exports.seed = async function (knex) {
       rider_id: 3,
       bike_components_id: 3,
     },
-  ]);
+  ];
+
+  const bikeData = [...bikeSeed];
+
+  const bikeNames = [
+    "Spoke & Mirrors",
+    "Wheelie Bad Bikes",
+    "Handlebar None",
+    "Two Tired Cycles",
+    "Pedal to the Meddle",
+    "Chain Reaction",
+    "Derailleur Swift",
+    "Tour de Farce",
+    "Crank Yankers",
+    "Freewheelin' Fools",
+    "Sprocket Science",
+    "Breaking Cadence",
+    "Unstable Cycles",
+  ];
+
+  const bikeModels = [
+    "Mongoose",
+    "Beaver Creek",
+    "Starburst",
+    "Ground Pounder",
+    "Mud Blaster",
+    "Kona Stuff",
+    "Kona Roast",
+    "Smut Raven",
+    "Pechango Crunk",
+    "Blitz Thunder",
+    "Sir Hurt Locker",
+    "She Shredder",
+    "Stompatron",
+  ];
+  const bikeStyles = [
+    "MTB",
+    "Road",
+    "Gravel",
+    "Touring",
+    "Fixed-Gear",
+    "City",
+    "Cargo",
+  ];
+  const bikeYear = [2001, 2006, 1987, 2025, 2026, 2021, 1991];
+
+  let growThis = 4;
+
+  for (let i = 0; i < 1000; i++) {
+    bikeSeed.forEach((bike) => {
+      const randomName =
+        bikeNames[Math.floor(Math.random() * bikeNames.length)];
+      const randomModel =
+        bikeModels[Math.floor(Math.random() * bikeModels.length)];
+      const randomStyle =
+        bikeStyles[Math.floor(Math.random() * bikeStyles.length)];
+      const randomYear = bikeYear[Math.floor(Math.random() * bikeYear.length)];
+
+      bikeData.push({
+        ...bike,
+        id: growThis,
+        make: randomName,
+        model: randomModel,
+        style: randomStyle,
+        year: randomYear,
+      });
+
+      growThis++;
+    });
+  }
+
+  await knex("bike").insert(bikeData);
 };
--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const logger = require("morgan");
+app.use(logger("tiny"));
 const port = 8081;
 
 const knex = require("knex")(require("../knexfile.js")["development"]);
@@ -14,6 +16,177 @@ app.get("/bikes", (req, res) => {
     .then((data) => {
       var bikeMakes = data.map((bike) => bike.make);
       res.json(bikeMakes);
+    });
+});
+
+app.get("/bikes/:id", (req, res) => {
+  knex("bike")
+    .join("bike_components", "bike.bike_components_id", "bike_components.id")
+    .where("bike.id", req.params.id)
+    .select("*")
+    .then((data) => {
+      if (data.length === 0) {
+        return res.status(404).json({ error: "Bike not found" });
+      }
+      var bikeInfo = data.map((details) => details);
+      res.json(bikeInfo);
+    });
+});
+
+app.post("/bikes", (req, res) => {
+  knex("bike")
+    .insert(req.body)
+    .returning("*")
+    .then((data) => {
+      res.status(201).json(data);
+    });
+});
+
+app.patch("/bikes/:id", (req, res) => {
+  knex("bike")
+    .where("id", req.params.id)
+    .update(req.body)
+    .returning("*")
+    .then((data) => {
+      if (data.length === 0) {
+        return res.status(404).json({ error: "Bike not found" });
+      }
+
+      res.json(data);
+    });
+});
+
+app.delete("/bikes/:id", (req, res) => {
+  knex("bike")
+    .where("id", req.params.id)
+    .del()
+    .returning("*")
+    .then((data) => {
+      if (data.length === 0) {
+        return res.status(404).json({ error: "Bike not found" });
+      }
+
+      res.json(data);
+    });
+});
+
+app.get("/riders", (req, res) => {
+  knex("rider")
+    .select("*")
+    .then((data) => {
+      var riderNames = data.map((rider) => rider.name);
+      res.json(riderNames);
+    });
+});
+
+app.get("/riders/:id", (req, res) => {
+  knex("rider")
+    .where("rider.id", req.params.id)
+    .select("*")
+    .then((data) => {
+      if (data.length === 0) {
+        return res.status(404).json({ error: "Rider not found" });
+      }
+      var riderInfo = data.map((details) => details);
+      res.json(riderInfo);
+    });
+});
+
+app.post("/riders", (req, res) => {
+  knex("rider")
+    .insert(req.body)
+    .returning("*")
+    .then((data) => {
+      res.status(201).json(data);
+    });
+});
+
+app.patch("/riders/:id", (req, res) => {
+  knex("rider")
+    .where("id", req.params.id)
+    .update(req.body)
+    .returning("*")
+    .then((data) => {
+      if (data.length === 0) {
+        return res.status(404).json({ error: "Rider not found" });
+      }
+
+      res.json(data);
+    });
+});
+
+app.delete("/riders/:id", (req, res) => {
+  knex("rider")
+    .where("id", req.params.id)
+    .del()
+    .returning("*")
+    .then((data) => {
+      if (data.length === 0) {
+        return res.status(404).json({ error: "Rider not found" });
+      }
+
+      res.json(data);
+    });
+});
+
+app.get("/components", (req, res) => {
+  knex("bike_components")
+    .select("*")
+    .then((data) => {
+      var bikeParts = data.map((parts) => parts);
+      res.json(bikeParts);
+    });
+});
+
+app.post("/components", (req, res) => {
+  knex("bike_components")
+    .insert(req.body)
+    .returning("*")
+    .then((data) => {
+      res.status(201).json(data);
+    });
+});
+
+app.get("/components/:id", (req, res) => {
+  knex("bike_components")
+    .where("id", req.params.id)
+    .select("*")
+    .then((data) => {
+      if (data.length === 0) {
+        return res.status(404).json({ error: "Components not found" });
+      }
+
+      var componentInfo = data.map((details) => details);
+
+      res.json(componentInfo);
+    });
+});
+
+app.patch("/components/:id", (req, res) => {
+  knex("bike_components")
+    .where("id", req.params.id)
+    .update(req.body)
+    .returning("*")
+    .then((data) => {
+      if (data.length === 0) {
+        return res.status(404).json({ error: "Components not found" });
+      }
+
+      res.json(data);
+    });
+});
+
+app.delete("/components/:id", (req, res) => {
+  knex("bike_components")
+    .where("id", req.params.id)
+    .del()
+    .returning("*")
+    .then((data) => {
+      if (data.length === 0) {
+        return res.status(404).json({ error: "Components not found" });
+      }
+
+      res.json(data);
     });
 });
 
